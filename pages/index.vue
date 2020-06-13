b8a1d337c08967e2aae37d61c3bf107bf4631794
thanapongsj1996 <thanapongsj1996@gmail.com>
2020-06-13 18:10:38 +0700

    Added coming soon animation

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
   <v-layout justify-center align-center>
     <div class="text-center">
-      <h3>A Part From The Book</h3>
-      <p>Coming soon...</p>
+      <img class="logo-img" src="~/assets/images/book.png" width="100" />
+      <div class="fade-header">
+        <h3>A Part From The Book</h3>
+        <p>Coming soon...</p>
+      </div>
     </div>
   </v-layout>
 </template>
@@ -16,3 +19,38 @@ export default {
   }
 }
 </script>
+
+<style>
+.logo-img {
+  opacity: 0.7;
+  animation-name: fadein;
+  animation-duration: 4s;
+  animation-iteration-count: 1;
+}
+.fade-header {
+  opacity: 0.7;
+  animation-name: fadein2;
+  animation-duration: 4s;
+  animation-iteration-count: 1;
+}
+
+@keyframes fadein {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 0.7;
+  }
+}
+@keyframes fadein2 {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 0.7;
+  }
+}
+</style>
